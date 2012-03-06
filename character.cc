af419edc98d92ed1cfa7b73ca8878adfe29740ce
Alex <eshink@gmail.com>
2012-03-06 21:26:51 +0000

    Move trie vaguely implemented, now testing hooks

--- character.cc
+++ character.cc
@@ -3,13 +3,21 @@
 character::character() 
 //Character constructor. This loads the whole character into memory so that that we don't have disk reads during gameplay
 {
-	head = new moveTrie();
+	moveTrie * curr;
+	move * temp;
+	temp = new move("A\0", 5);
+	head = new moveTrie(temp);
+	curr = head->insert(6);
+	temp = new move("236A\0", 236);
+	curr = curr->insert(3);
+	curr = curr->insert(2, temp); 
 	//Empty for now
 }
 
 character::~character()
 //Character destructor. Might not need this if we aren't working with any dynamic memory, but it might be worthwhile to have.
 {
+	delete head;
 	//Empty for now
 }
 
--- hooks.cc
+++ hooks.cc
@@ -3,13 +3,14 @@
 void interface::pushInput(int axis[4], int button[5])
 {
 	int temp = 5 + axis[0]*3 - axis[1]*3 - axis[2] + axis[3];
-
+	move * t = NULL;
 //	if(temp != 5){	
 
 	inputBuffer[0] = temp;
-	
-//	pick.head.moveHook(0);
 
+	if(button[0] == 1) t = pick->head->moveHook(inputBuffer, 0);
+	if(t != NULL) t->execute();
+	
 	for(int i = 15; i >= 0; i--){
 		if(inputBuffer[i] != 0) inputBuffer[i+1] = inputBuffer[i];
 	}
@@ -22,17 +23,18 @@ void interface::pushInput(int axis[4], int button[5])
 */
 }
 
-move * moveTrie::moveHook(int i, int meter, int state)
+move * moveTrie::moveHook(int inputBuffer[16], int i)
 {
-	move * test;
-	if(child != NULL){
-		for(i; i < 16; i++){
-			for(int j = 0; j < children; j++)
-				test = child[j]->moveHook(i, meter, state);
-			if(test != NULL) return test;
+	moveTrie * test;
+	move * result;
+	for(i; i < 16; i++){
+		test = child[inputBuffer[i]];
+		if(test != NULL){
+			result = test->moveHook(inputBuffer, i);
+			if(result != NULL) return result;
 		}
 	}
 	if(fish != NULL) 
-		if(fish->check(meter, state) == 1) return fish;
+		if(fish->check(0, 0) == 1) return fish;
 	return NULL;
 }
