71e2efa5950159b9f289441fd87cbb7421b8cd3d
Alex <eshink@gmail.com>
2012-03-12 18:59:48 +0000

    While I'm at it, may as well make a real animation

--- red.cc
+++ red.cc
@@ -2,4 +2,10 @@
 red::red()
 {
 	name = "Red\0";
+	a5A = new frame("Red/A\0", 10);
+}
+
+void red::m5A(int f, SDL_Rect &collision, SDL_Rect &hitBox, SDL_Rect &hittable, frame *& sprite)
+{
+	if(f = 0) sprite = a5A;
 }
--- red.h
+++ red.h
@@ -1,5 +1,6 @@
 #include "character.h"
 class red : public character{
 public:
-	red();		
+	red();
+	void m5A(int, SDL_Rect&, SDL_Rect&, SDL_Rect&, frame *&);
 };
